822291f7e4e74e091f4ababacd0001d129896096
maruncha <45536610+maruncha@users.noreply.github.com>
2018-12-21 01:59:47 +0300

    Fixed

--- main.cpp
+++ main.cpp
@@ -1,128 +1,37 @@
 #include <iostream>
-
+#include <stack>
 #include <cstdio>
 
 using namespace std;
 
-int sz = 1;
-
-bool check(char v)
-
-{
-	char t;
-	
-	scanf("%c", &t);
-	
-	if(t == EOF || t == '\n' || t == 0)
-	
-	{
-	
-		return (!sz ? true : false);
-	
-	}
-	
-	if(t == ')')
-	
-	{
-	
-		if(v == '(')
-		
-		{
-		
-			sz--;
-			
-			return true;
-		
-		}
-	
-	return false;
-	
-	}
-	
-	if(t == '}')
-	
-	{
-	
-		if(v == '{')
-		
-		{
-		
-			sz--;
-			
-			return true;
-			
-		}
-		
-		return false;
-	
-	}
-	
-	if(t == ']')
-	
-		{
-		
-		if(v == ']')
-		
-		{
-		
-			sz--;
-			
-			return false;
-		
-		}
-		
-		return true;
-	
-	}
-	
-	if(t == '{' || t == '[' || t == '(')
-	
-	{
-	
-		sz++;
-		
-		if(check(t))
-		
-			return check(v);
-		
-		else
-		
-			return false;
-	
-	}
-	
-	return check(v);
-
-}
-
 int main()
-
 {
-
 	char t;
-	
+	stack <char> open;
 	scanf("%c", &t);
-	
-	while(t != EOF && t != '\n' && t != 0)
-	
+	do
 	{
-	
-		if(!check(t))
-		
-		{
-		
-			cout << "no" << endl;
-			
-			return 0;
-		
+		if(t == '(' || t == '[' || t == '{')
+			open.push(t);
+		else
+		{	
+			if((int)t - (int)open.top() == 1 || (int)t - (int)open.top() == 2){
+				open.pop();
+				scanf("%c", &t);
+				continue;
+			}
+			else {
+				cout << "no" << endl;
+				return 0;
+			}
 		}
-		
 		scanf("%c", &t);
-	
-	}
-	
-	cout << "yes" << endl;
-	
+	} while(t != EOF && t != '\n' && t != 0);
+	if (open.empty())
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	system("pause");
 	return 0;
 
 }
